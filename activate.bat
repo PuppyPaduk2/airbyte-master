d97594fcd409b643055ca31d65180be85995b402
PuppyPaduk2 <debraooclark@gmail.com>
2023-05-02 22:22:54 -0400

    Test verion committed

--- activate.bat
+++ activate.bat
@@ -3,5 +3,5 @@ git config user.name PuppyPaduk2
 git config user.email debraooclark@gmail.com
 git add -A
 git commit -m "Test verion committed"
-git remote add origin https://github.com/PuppyPaduk2/airbyte-master.git
+git remote add origin https://github.com/PuppyPaduk2/best-of-ml-python-main.git
 git push -u origin master
